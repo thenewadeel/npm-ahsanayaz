953df3b6ba9209bf9b3e0b401336108678b3e894
thenewadeel <thenewadeel@gmail.com>
2019-11-20 00:39:43 +0500

    finished

--- index.js
+++ index.js
@@ -3,21 +3,24 @@
 const {
   white,
   bold,
-  blueBright
+  blueBright,
+  green,
+  red,
+  greenBright,
 } = require("colorette");
 
-const work = white(`Software Engineer @ ${blueBright('Modus Create')}`)
+const work = greenBright(`Software Engineer @ ${red('Home Sweet Home <3')}`)
 //const gde = `${blueBright('  Google Developers Expert')} ${white('in Angular')}`
 const twitter = blueBright('https://twitter.com/thenewadeel')
 const linkedIn = blueBright('https://linkedin.com/in/thenewadeel')
 const github = blueBright('https://github.com/thenewadeel')
 const slides = blueBright('https://slides.com/thenewadeel')
 const medium = blueBright('https://medium.com/@thenewadeel')
-const npx = white('npx thenewadeel')
+const npx = green('npx thenewadeel')
 
 const newline = '\n'
-const heading = `${white('  Muhammad Adeel Shahid /')} ${blueBright('ahsanayaz')}`
-//const working = `${white(bold('     Work:'))}  ${work}`
+const heading = `${white('  Muhammad Adeel Shahid ')}`
+const working = `${white(bold('     Work:'))}  ${work}`
 const linkedining = `${white(bold('  LinkedIn:'))} ${linkedIn}`
 const twittering = `${white(bold('  Twitter:'))}  ${twitter}`
 const githubing = `${white(bold('   GitHub:'))}  ${github}`
@@ -25,6 +28,6 @@ const slidesing = `${white(bold('   Slides:'))}  ${slides}`;
 const mediuming = `${white(bold('   Medium:'))}  ${medium}`;
 const carding = `${white(bold('     Card:'))}  ${npx}`
 
-let output = `${newline} ${newline} ${heading} ${newline} ${gde} ${newline} ${newline} ${working} ${newline} ${twittering} ${newline} ${linkedining} ${newline} ${githubing} ${newline} ${slidesing} ${newline} ${mediuming} ${newline} ${newline} ${carding} ${newline} ${newline}`;
+let output = `${newline} ${newline} ${heading} ${newline} ${newline}  ${working} ${newline} ${twittering} ${newline} ${linkedining} ${newline} ${githubing} ${newline} ${slidesing} ${newline} ${mediuming} ${newline} ${newline} ${carding} ${newline} ${newline}`;
 
 console.log(output)
